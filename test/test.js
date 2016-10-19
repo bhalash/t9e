11916bb0b048d7ea533d7a83d8056485e1f18e65
Mark <mark@bhalash.com>
2016-10-19 19:46:57 +0100

    Completed test suite.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,42 @@
 const tape = require('tape');
 const t9e = require('../');
 
+const chars = { win: 'X', lose: 'O' };
+
+const boards = {
+    wins: {
+        row: [
+            ['O', 'X', 'O'],
+            ['X', 'X', 'X'],
+            ['X', 'O', 'O']
+        ],
+        column: [
+            ['X', 'O', 'O'],
+            ['X', 'O', 'X'],
+            ['X', 'X', 'O']
+        ],
+        diagonal: {
+            forward: [
+                ['X', 'O', 'O'],
+                ['O', 'X', 'O'],
+                ['X', 'O', 'X']
+            ],
+            backward: [
+                ['O', 'O', 'X'],
+                ['O', 'X', 'O'],
+                ['X', 'X', 'O']
+            ]
+        }
+    },
+    falsy: [
+        ['X', 'O', 'X'],
+        ['O', 'O', null],
+        ['X', 'X', 'O']
+    ],
+    empty: t9e.board(5, null),
+    play: t9e.board(3)
+};
+
 /**
  * board() Method
  * =============================================================================
@@ -82,12 +118,60 @@ tape('board()', assert => {
  */
 
 tape('check()', assert => {
-    assert.plan(1);
+    assert.plan(9);
 
     assert.equal(
         typeof t9e.check,
         'function',
         'Should be a function.'
+    );
+
+    assert.equal(
+        true,
+        t9e.check(chars.win, boards.wins.row),
+        'Should detect win conditions on a row.'
+    );
+
+    assert.equal(
+        true,
+        t9e.check(chars.win, boards.wins.column),
+        'Should detect win conditions on a column.'
+    );
+
+    assert.equal(
+        true,
+        t9e.check(chars.win, boards.wins.diagonal.forward),
+        'Should detect win conditions on a forward diagonal.'
+    );
+
+    assert.equal(
+        true,
+        t9e.check(chars.win, boards.wins.diagonal.backward),
+        'Should detect win conditions on a backward diagonal.'
+    );
+
+    assert.equal(
+        false,
+        t9e.check(chars.win, boards.wins.falsy),
+        'Should not detect win conditions on an unfilled board.'
+    );
+
+    assert.equal(
+        false,
+        t9e.check(chars.win, boards.wins.falsy),
+        'Should not detect win conditions on an unfilled board.'
+    );
+
+    assert.equal(
+        false,
+        t9e.check(chars.win, []),
+        'Should not detect win conditions on an empty array.'
+    );
+
+    assert.equal(
+        false,
+        t9e.check(chars.win, null),
+        'Should not detect win conditions on an invalid value.'
     );
 });
 
@@ -97,11 +181,29 @@ tape('check()', assert => {
  */
 
 tape('check()', assert => {
-    assert.plan(1);
+    assert.plan(4);
 
     assert.equal(
         typeof t9e.anyEmpty,
         'function',
         'Should be a function.'
+    );
+
+    assert.equal(
+        true,
+        t9e.anyEmpty(boards.falsy),
+        'Should detect empty cells on an unfilled board.'
+    );
+
+    assert.equal(
+        true,
+        t9e.anyEmpty(boards.empty),
+        'Should detect empty cells on an empty board.'
+    );
+
+    assert.equal(
+        false,
+        t9e.anyEmpty(boards.wins.row),
+        'Should detect no empty cells on a filled board.'
     );
 });
